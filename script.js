8bad9b80e0137f0b68fa25edae8e98dc846a67a3
BalaussaSatymbek <satymbek07@mail.ru>
2026-02-22 12:48:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -34,9 +34,4 @@ function calculateCoffee() {
 
   // 7. Show result on the page
   document.getElementById("result").innerHTML = resultText;
-}// Starter JS
-// Steps for students to implement:
-// 1. Get user inputs
-// 2. Perform calculations
-// 3. Use if/else to classify results
-// 4. Display results in #output
+}
